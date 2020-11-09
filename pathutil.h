d53abb7a555ac04be042bf069cb606b89de4adc6
Stephen Punak <stephen.punak@lordstownmotors.com>
2020-11-09 13:38:56 -0800

    Added PathUtil::combine

--- pathutil.h
+++ pathutil.h
@@ -1,6 +1,7 @@
 #ifndef PATHUTIL_H
 #define PATHUTIL_H
 #include <QString>
+#include <QDir>
 
 class PathUtil
 {
@@ -8,6 +9,10 @@ public:
     static QString fileName(const QString& path);
     static QString dirName(const QString& path);
     static QString trimTrailingSlash(const QString& path);
+    static QString combine(const QString& p1, const QString& p2) { return QDir::cleanPath(p1 + '/' + p2); }
+    static QString combine(const QString& p1, const QString& p2, const QString& p3) { return QDir::cleanPath(p1 + '/' + p2 + '/' + p3); }
+    static QString combine(const QString& p1, const QString& p2, const QString& p3, const QString& p4) { return QDir::cleanPath(p1 + '/' + p2 + '/' + p3 + '/' + p4); }
+    static QString combine(const QString& p1, const QString& p2, const QString& p3, const QString& p4, const QString& p5) { return QDir::cleanPath(p1 + '/' + p2 + '/' + p3 + '/' + p4 + '/' + p5); }
 };
 
 #endif // PATHUTIL_H
