429d02fde8c237161f16fdc5eeb310c951533d25
Stephen Punak <stephen.punak@lordstownmotors.com>
2021-02-07 21:52:16 -0800

    General date string parse

--- datetimeutil.cpp
+++ datetimeutil.cpp
@@ -1,2 +1,13 @@
 #include "datetimeutil.h"
 
+
+QDateTime DateTimeUtil::fromString(const QString &date)
+{
+    QDateTime result;
+    if( (result = fromISOString(date)).isValid() ||
+        (result = fromStandardString(date)).isValid() ||
+        (result = QDateTime::fromString(date)).isValid())
+    {
+    }
+    return result;
+}
