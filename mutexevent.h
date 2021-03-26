da66426b73002ce939d19d8fbc1e2d9deaa67e97
Stephen Punak <stephen.punak@lordstownmotors.com>
2021-03-26 09:48:13 -0700

    Add multi-waiter functionality into mutex event

--- mutexevent.h
+++ mutexevent.h
@@ -8,19 +8,24 @@
 class KANOOP_EXPORT MutexEvent
 {
 public:
-    MutexEvent();
+    MutexEvent(bool wakeAllWaiters = false) :
+        _isset(false),
+        _debug(false),
+        _wakeAllWaiters(wakeAllWaiters) {}
 
     void set();
     void clear();
     bool wait(int msecs = 0);
 
     void setDebug() { _debug = true; }
+    void setWakeAllWaiters(bool value) { _wakeAllWaiters = value; }
 
 private:
     QWaitCondition  _condition;
     QMutex _checkMutex;
     bool _isset;
     bool _debug;
+    bool _wakeAllWaiters;
 };
 
 #endif // MUTEXEVENT_H
