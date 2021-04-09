da852b1cc499e402738b2d7eb7204879759c9497
Stephen Punak <stephen.punak@lordstownmotors.com>
2021-04-09 16:35:08 -0700

    Added stringlist to string conversions

--- stringutil.cpp
+++ stringutil.cpp
@@ -1,2 +1,16 @@
 #include "stringutil.h"
+#include <QTextStream>
 
+QString StringUtil::toDelimitedString(const QStringList &list, char delimiter)
+{
+    QString outputString;
+    outputString.reserve(list.size() * 256);
+    QTextStream output(&outputString);
+    for(int i = 0;i < list.count();i++)
+    {
+        output << list.at(i);
+        if(i < list.count() - 1)
+            output << delimiter;
+    }
+    return outputString;
+}
--- stringutil.h
+++ stringutil.h
@@ -10,6 +10,7 @@ public:
     static bool toBool(const QString& value) { return value.toLower() == "true" || value == "1"; }
     static bool toBool(int value) { return value != 0; }
     static QString toString(bool value) { return value ? "true" : "false"; }
+    static QString toDelimitedString(const QStringList& list, char delimiter = ' ');
 };
 
 #endif // STRINGUTIL_H
