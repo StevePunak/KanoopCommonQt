31397cd5124cc6aa629275cc1acc6f55aca0fb05
Stephen Punak <stephen.punak@lordstownmotors.com>
2022-04-12 13:31:41 -0700

    Thread strings now conform to Qt debugger

--- threadutil.h
+++ threadutil.h
@@ -7,8 +7,8 @@
 class KANOOP_EXPORT ThreadUtil
 {
 public:
-    static QString currentThreadString() { return QString("0x%1").arg((quint64)QThread::currentThreadId(), 8, 16, QChar('0')); }
-    static QString threadString(QThread* thread) { return QString("0x%1").arg((quint64)thread, 8, 16, QChar('0')); }
+    static QString currentThreadString();
+    static QString threadString(QThread* thread);
 };
 
 #endif // THREADUTIL_H
