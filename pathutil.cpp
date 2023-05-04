82093d37e8774f37b5e5f96ef198511fd35f8b29
Alok Kumar Bajpai <alok.bajpai@lordstownmotors.com>
2023-05-04 16:37:10 -0700

    Using QFileInfo to parse the fileName properly

--- pathutil.cpp
+++ pathutil.cpp
@@ -5,14 +5,8 @@
 
 QString PathUtil::fileName(const QString &path)
 {
-    QString ret;
-    int index = path.lastIndexOf(QDir::separator());
-    if(index < 0)
-        ret = path;
-    else
-        ret = path.mid(index + 1);
-
-    return ret;
+    QFileInfo info(path);
+    return info.fileName ();
 }
 
 QString PathUtil::dirName(const QString &path)
